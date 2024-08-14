81a0c7bed992845ac77507d8bd2f21ba86dd6913
shaikafrid143143 <70587992+shaikafrid143143@users.noreply.github.com>
2024-08-14 14:48:12 +0000

    removed mongodb and models folder

--- src/mongoDB/credentials.ts
+++
@@ -1,5 +0,0 @@
-
-
-export function getMongoUrl(collectionName: string):string {
-    return `mongodb+srv://afriddev:17030143@cluster0.6t6dm.mongodb.net/${collectionName}?retryWrites=true&w=majority&appName=cluster0`
-}
