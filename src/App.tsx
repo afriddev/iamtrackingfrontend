a9e251de9d7cfac8588a266c80df527c3324163f
shaikafrid143143 <70587992+shaikafrid143143@users.noreply.github.com>
2024-08-14 14:18:57 +0000

    changed complete ui fron nexjs to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
+import {
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import HomePage from "./components/home/HomePage";
+import { useAppContext } from "./utils/AppContext";
+import Loading from "./utils/Loading";
+import { useGetAndSetUserData } from "./hooks/userHooks";
 
-function App() {
-  return <></>;
+export default function App() {
+  const [pageindex, setPageIndex] = useState<number>(0);
+  const { isLoading } = useAppContext()
+  function handlePageIndexChange(pageNumber: number) {
+    setPageIndex(pageNumber);
+  }
+  
+
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+        {(isLoading ) && <Loading />}
+        <div className="relative font-mono">
+          <div className="absolute  z-[1] h-[100vh] w-[100vw] ">
+            <img src="beams.jpg" className="w-full h-full object-cover" />
+          </div>
+
+          <div className="z-[2]  relative top-1  w-[100vw]">
+            <HomePage setPageNumber={handlePageIndexChange} />
+          </div>
+        </div>
+    </QueryClientProvider>
+  );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import AppContext from './utils/AppContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <AppContext> <App /></AppContext>
 )
